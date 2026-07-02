f7d05e059ea877a7afaf5c9418bfb576eb080ad9
vasanthsingh <vasanthrasaputhra@gmail.com>
2026-07-02 17:34:02 +0530

    Add initial project structure and configuration for LibraryManagement
    - Created .classpath and .project files for Eclipse IDE integration.
    - Added Maven configuration in pom.xml with dependencies for Spring and JUnit.
    - Implemented main application class (App) and service class (BookService) with a repository (BookRepository).
    - Created application context XML for Spring bean configuration.
    - Added unit tests for the application.
    - Configured encoding and compiler settings in Eclipse preferences.

---
+++ Deep Skilling/Week - 2/Spring Core and Maven/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.library.repository;
+
+public class BookRepository {
+	public void displayBook() {
+        System.out.println("Book Repository: Book Data Retrieved");
+    }
+}
---
+++ Deep Skilling/Week - 2/Spring Core and Maven/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,15 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+	private BookRepository repository;
+
+    public void setRepository(BookRepository repository) {
+        this.repository = repository;
+    }
+    
+    public void showBook() {
+        repository.displayBook();
+    }
+}
